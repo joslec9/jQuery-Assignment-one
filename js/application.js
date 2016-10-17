5edde473899784a35605ee9ddd3e30ce7a29a0b8
Jose Lechuga <joslec9@gmail.com>
2016-10-17 00:38:16 -0400

    update
    update

--- js/application.js
+++ js/application.js
@@ -27,38 +27,38 @@ $(document).ready(function () {
     });
 
     // Insert content, specified by the parameter, to the end of each element in the set of matched elements.
-    $('.append').click(function () {
-        $('#append').append("This has been appended!!!");
+    $(".append").click(function () {
+        $("#append").append("This has been appended!!!");
     });
 
     // Insert content, specified by the parameter, after each element in the set of matched elements.
-    $('.after').click(function () {
-        $('#after').after("WAHOO!!!");
+    $(".after").click(function () {
+        $("#after").after("OH Yeah");
     });
 
     //Get the value of an attribute for the first element in the set of matched elements or set one or more attributes for every matched element.
-    $('.attr').click(function () {
-        $("#attr img").attr('src', '../images/subway_image.jpg');
+    $(".attr").click(function () {
+        $("#attr img").attr("src", "../images/subway_image.jpg");
     });
 
     //Insert content, specified by the parameter, before each element in the set of matched elements.
-    $('.before').click(function () {
+    $(".before").click(function () {
         $("#before").before("This is kicking my butt...");
     });
 
     //Get the HTML contents of the first element in the set of matched elements or set the HTML contents of every matched element.
-    $('.html').click(function () {
-        $('#html').html("This is the html method, which replaces all the text inside my p tag");
+    $(".html").click(function () {
+        $("#html").html("This is the html method, which replaces all the text inside my p tag");
     });
 
     // Get the combined text contents of each element in the set of matched elements, including their descendants, or set the text contents of the matched elements.
-    $('.text').click(function () {
-        $('#text').text("Ribs tip venison, ham spare chop spare capicola ham andouille pork bresaola");
+    $(".text").click(function () {
+        $("#text").text("Ribs tip venison, ham spare chop spare capicola ham andouille pork bresaola");
     });
 
     //Get the current value of the first element in the set of matched elements or set the value of every matched element.
-    $('.val').click(function () {
-        alert($('input:text').val());
+    $(".val").click(function () {
+        alert($("input:text").val());
     });
     
     //Adds the specified class(es) to each element in the set of matched elements.    
@@ -68,11 +68,11 @@ $(document).ready(function () {
     
     //Display or hide the matched elements with a sliding motion.
     $(".slideToggle").click(function () {
-        $('#slide_toggle').slideToggle();
+        $("#slide_toggle").slideToggle();
     });
 
     //Hide the matched elements.
-    $('.hide').click(function () {
+    $(".hide").click(function () {
         $("#hide_show").hide();
     });
 
@@ -87,13 +87,13 @@ $(document).ready(function () {
     });
     
     //Bind an event handler to the “focus” JavaScript event, or trigger that event on an element.
-    $('.focus').click(function () {
-        $('#focus').focus();
+    $(".focus").click(function () {
+        $("#focus").focus();
     });
     
     //Bind an event handler to the “click” JavaScript event, or trigger that event on an element.
-    $('.click').click(function () {
-        $('focus').click();
+    $(".click").click(function () {
+        $("focus").click();
     });
 
 });
